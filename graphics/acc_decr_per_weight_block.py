2b8a784eb400a76f22e2db10da6463d5790b0164
kirrog <kirrog@zertan.ru>
2023-05-24 08:36:58 +0300

    feature: accuracy drop per weight for every coef

--- graphics/acc_decr_per_weight_block.py
+++ graphics/acc_decr_per_weight_block.py
@@ -39,22 +39,29 @@ def acc_per_weight_graphic(data_metrics, name="", model_num_of_weights=14505354)
                 best_epoch_num_3 = j
 
         parameters_best_cut_0_0.append((epochs[best_epoch_num_0]["block"]["orig"]["acc"] - max_cut_acc_0) / (
-                    model_num_of_weights - epochs[best_epoch_num_0]["block"]["steps"][0][2]))
+                model_num_of_weights - epochs[best_epoch_num_0]["block"]["steps"][0][2]))
         parameters_best_cut_0_1.append((epochs[best_epoch_num_1]["block"]["orig"]["acc"] - max_cut_acc_1) / (
-                    model_num_of_weights - epochs[best_epoch_num_1]["block"]["steps"][1][2]))
+                model_num_of_weights - epochs[best_epoch_num_1]["block"]["steps"][1][2]))
         parameters_best_cut_1_0.append((epochs[best_epoch_num_2]["block"]["orig"]["acc"] - max_cut_acc_2) / (
-                    model_num_of_weights - epochs[best_epoch_num_2]["block"]["steps"][2][2]))
+                model_num_of_weights - epochs[best_epoch_num_2]["block"]["steps"][2][2]))
         parameters_best_cut_1_1.append((epochs[best_epoch_num_3]["block"]["orig"]["acc"] - max_cut_acc_3) / (
-                    model_num_of_weights - epochs[best_epoch_num_3]["block"]["steps"][3][2]))
+                model_num_of_weights - epochs[best_epoch_num_3]["block"]["steps"][3][2]))
         coefficient.append(i + 1)
 
     plt.plot(coefficient, parameters_best_cut_0_0, label="Best_0")
     plt.plot(coefficient, parameters_best_cut_0_1, label="Best_1")
+
+    plt.legend(loc="lower right")
+    plt.xlabel("per weight")
+    plt.ylabel("accuracy drop")
+    plt.title(name)
+    plt.show()
+
     plt.plot(coefficient, parameters_best_cut_1_0, label="Best_2")
     plt.plot(coefficient, parameters_best_cut_1_1, label="Best_3")
     plt.legend(loc="lower right")
-    plt.xlabel("experiment number")
-    plt.ylabel("accuracy")
+    plt.xlabel("per weight")
+    plt.ylabel("accuracy drop")
     plt.title(name)
     plt.show()
 
