c5bfb5cb7d07f385ac2526a6d7650b7e0e42f8a1
kirrog <kirrog@zertan.ru>
2024-04-11 10:06:24 +0300

    feature: graphics of comparing

--- graphics/acc_per_filter_for_coef_filter.py
+++ graphics/acc_per_filter_for_coef_filter.py
@@ -9,16 +9,19 @@ def accuracy_cut_coefficient_graphic(data_metrics, name=""):
     accuracy = []
     cutted_filters = []
     labels = []
-    for i, (k, v) in enumerate(sorted(data_metrics.items(), key=lambda x: int(x[0][6:8]))):
+    for i, (k, v) in enumerate(sorted(data_metrics.items(), key=lambda x: float(x[0].split("_")[1]))):
         acc = []
         filt = []
         for j, value in enumerate(v):
             cut_acc = value["filter"]["results"]["exp_metrics"]["acc"]
-            cut_weights = sum([(x[2]) * (y // 4) for x, y in
-                               zip(value["filter"]["results"]["results_cut"], value["filter"]["results"]["mids"])])
+            cut_weights = sum([x * (y // 4) for x, y in
+                               zip([576, 576, 4608, 4608], value["filter"]["results"]["mids"])])
+
+            # cut_weights = sum([(x[2]) * (y // 4) for x, y in
+            #                    zip(value["filter"]["results"]["results_cut"], value["filter"]["results"]["mids"])])
             acc.append(cut_acc)
             filt.append(cut_weights)
-        labels.append(i+1)
+        labels.append(i + 1)
         accuracy.append(acc)
         cutted_filters.append(filt)
     for acc, filt, label in zip(accuracy, cutted_filters, labels):
@@ -32,6 +35,7 @@ def accuracy_cut_coefficient_graphic(data_metrics, name=""):
 
 def accuracy_cut_coefficient_graphics():
     o, b, g = load_metrics(Path("/home/kirrog/projects/FQWB/model/stats_pool"))
+    accuracy_cut_coefficient_graphic(o, "Original")
     accuracy_cut_coefficient_graphic(b, "Block")
     accuracy_cut_coefficient_graphic(g, "Filter")
 
---
+++ graphics/functions_correlations.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+
+from graphics.base import load_metrics
+
+
+def weights_correlation(data_metrics, default_dict, entropy_dict, name=""):
+    for k in set(data_metrics.keys()).intersection(default_dict.keys()).intersection(entropy_dict.keys()):
+        reg_data, default_data, entropy_data = data_metrics[k], default_dict[k], entropy_dict[k]
+        best_acc = list(sorted((set(map(lambda x: x["filename"], reg_data))
+                                .intersection(map(lambda x: x["filename"], default_data))
+                                .intersection(map(lambda x: x["filename"], entropy_data))),
+                               key=lambda x: float(x.split("_")[3][:-4])))[-1]
+        reg_case = list(filter(lambda x: x["filename"] == best_acc, reg_data))[0]
+        def_case = list(filter(lambda x: x["filename"] == best_acc, default_data))[0]
+        ent_case = list(filter(lambda x: x["filename"] == best_acc, entropy_data))[0]
+        for step_reg, step_case, step_ent, label_num in zip(reg_case["filter"]["steps"],
+                                                            def_case["filter"]["steps"],
+                                                            ent_case["filter"]["steps"],
+                                                            range(len(ent_case["filter"]["steps"]))):
+            x = list(range(len(step_reg[2])))
+            norm = max([elem[1] for elem in step_reg[2]])
+            y = [elem[1] / norm for elem in step_reg[2]]
+            xy = [(x, y) for x, y in sorted(zip(x, y), key=lambda elem: elem[1])]
+            order = [x for x, y in xy]
+            y_ordered = [y[o] for o in order]
+
+            plt.plot(y_ordered, "o", label="Weights")
+
+            norm_max = max([abs(elem[1]) for elem in step_case[2]])
+            norm_min = min([abs(elem[1]) for elem in step_case[2]])
+            y = [(abs(elem[1]) - norm_min) / (norm_max - norm_min) for elem in step_case[2]]
+            y_ordered = [y[o] for o in order]
+
+            plt.plot(y_ordered, "o", label="Rademacher")
+
+            norm = max([elem[1] for elem in step_ent[2]])
+            y = [elem[1] / norm for elem in step_ent[2]]
+            y_ordered = [y[o] for o in order]
+
+            plt.plot(y_ordered, "o", label="Entropy")
+
+            plt.legend(loc="upper right", numpoints=1)
+            plt.xlabel("Group number")
+            plt.ylabel("Feature value")
+            plt.title(f"Exp: {name} type: {k} ResBlock number: {label_num}")
+            plt.show()
+
+
+def accuracy_cut_coefficient_graphics():
+    o_p, b_p, g_p = load_metrics(Path("/home/kirrog/projects/FQWB/model/stats_pool"))
+    o_d, b_d, g_d = load_metrics(Path("/home/kirrog/projects/FQWB/model/stats_radamcher_default"))
+    o_e, b_e, g_e = load_metrics(Path("/home/kirrog/projects/FQWB/model/stats_radamcher_entropy"))
+    weights_correlation(o_p, o_d, o_e, "Original")
+    weights_correlation(b_p, b_d, b_e, "Block")
+    weights_correlation(g_p, g_d, g_e, "Filter")
+
+
+if __name__ == "__main__":
+    accuracy_cut_coefficient_graphics()
