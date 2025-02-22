58ed650de7163df7bb15f763bc54a9d3c4cc96a0
kirrog <kirrog@zertan.ru>
2025-02-22 17:33:33 +0300

    [feature]: form graphics of compare

--- graphics/acc_inc_speed_diagram.py
+++ graphics/acc_inc_speed_diagram.py
@@ -1,5 +1,6 @@
 import json
-from statistics import mean
+from collections import defaultdict
+from pprint import pprint
 
 from matplotlib import pyplot as plt
 
@@ -8,7 +9,6 @@ with open("prepared_cuttings_stats.json", "r", encoding="utf-8") as f:
 
 bar_val_list = []
 color_list = []
-
 
 init_type2color = {
     "raw": "yellow",
@@ -30,12 +30,15 @@ weight_reg_func_name2color = {
 
 inner_reg_func_name2color = {
     "none": "c",
-    "radem-inv": "red", # (0.0, 1.0, 0.0, 0.3),
-    "radem": "red", # (0.0, 1.0, 0.0, 1.0),
-    "entr-inv": "blue", # (0.0, 0.0, 1.0, 0.3),
-    "entr": "blue", # (0.0, 0.0, 1.0, 1.0),
+    "radem-inv": "red",  # (0.0, 1.0, 0.0, 0.3),
+    "radem": "red",  # (0.0, 1.0, 0.0, 1.0),
+    "entr-inv": "blue",  # (0.0, 0.0, 1.0, 0.3),
+    "entr": "blue",  # (0.0, 0.0, 1.0, 1.0),
     "l1_l2": 'black',
 }
+
+increment_stats = defaultdict(float)
+increment_stats_num = defaultdict(int)
 
 for exp_params, exp_stats in experiments_dict.items():
     (init_type, epoch, weight_reg_func_name,
@@ -60,11 +63,25 @@ for exp_params, exp_stats in experiments_dict.items():
         continue
     color_list.append(inner_reg_func_name2color[inner_reg_func_name])
     bar_val_list.append((sum(acc_inc_speed_grow_list) / len(acc_inc_speed_grow_list)))
+    increment_stats[inner_reg_func_name] += sum(acc_inc_speed_grow_list) / len(acc_inc_speed_grow_list)
+    increment_stats_num[inner_reg_func_name] += 1
 
-plt.bar(list(range(len(bar_val_list))), bar_val_list, color=color_list)
+# plt.bar(list(range(len(bar_val_list))), bar_val_list, color=color_list)
+# plt.ylabel("Accuracy")
+# plt.xlabel("Weights deleted")
+#
+# plt.title(f"Params reg func compare for l1 l2")
+# plt.show()
+# plt.savefig(f"../graphics_data/cuttings_compare/init_strategy_compare.png")
+
+incr_stats_list = list(increment_stats.items())
+pprint([func_name for func_name, stats_val in incr_stats_list])
+plt.bar(list(range(len(incr_stats_list))),
+        [stats_val/ increment_stats_num[func_name] for func_name, stats_val in incr_stats_list],
+        color=[inner_reg_func_name2color[func_name] for func_name, stats_val in incr_stats_list])
+plt.legend([func_name for func_name, stats_val in incr_stats_list])
 plt.ylabel("Accuracy")
 plt.xlabel("Weights deleted")
 
 plt.title(f"Params reg func compare for l1 l2")
 plt.show()
-# plt.savefig(f"../graphics_data/cuttings_compare/init_strategy_compare.png")
